1e912c030a916f6a29edbc8b24ce1854bf96ab7c
Koichi Kataoka <k.koichi276052@gmail.com>
2024-09-26 18:11:47 +0900

    john comfy store wip

--- comfy-store/src/pages/HomeLayout.tsx
+++ comfy-store/src/pages/HomeLayout.tsx
@@ -1,11 +1,20 @@
 import { Header } from '@/components';
 import { Outlet } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 
 function HomeLayout() {
   return (
     <>
-      <Header/>
-      <nav>navbar</nav>
+      <Header />
+      <div>
+        <Button
+          variant="destructive"
+          size="lg"
+          onClick={() => console.log('it worked!!!')}
+        >
+          Click Me
+        </Button>
+      </div>
       <Outlet />
     </>
   );
